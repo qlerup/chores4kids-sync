53054bac4dfff041401f3cbbbce23dfef54f799c
Glerup <k3dd3@hotmail.dk>
2025-10-19 15:06:24 +0200

    Update config_flow.py

--- custom_components/chores4kids/config_flow.py
+++ custom_components/chores4kids/config_flow.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
+import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.data_entry_flow import FlowResult
 from .const import DOMAIN
 
-class SimpleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_user(self, user_input=None) -> FlowResult:
-        if user_input is None:
-            return self.async_show_form(step_id="user")
-        return self.async_create_entry(title="Chores4Kids", data={})
+class Chores4KidsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            # Ingen konfiguration nødvendig – bare opret entry
+            return self.async_create_entry(title="Chores4Kids", data={})
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
